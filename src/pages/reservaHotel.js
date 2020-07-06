54d1b0a897fee80bf69fc78e7faf143e2f28e0e9
Márcio De Andrade Sena <marcio.as@outlook.com>
2020-07-06 18:42:31 -0300

    dados de contrato

--- src/pages/reservaHotel.js
+++ src/pages/reservaHotel.js
@@ -185,14 +185,9 @@ export default () => {
             `${userInput.name}: *${userInput.value}* `
         )).join('%0a');
         const msg = `Ola! %0a Solicito hospedagem ${hasTaxiDog?'*com taxi dog*':''} para: %0a ${textPets} %0a ${textValor} %0a Meu Dados: %0a ${TextProprietary}`;
-        window.open(`https://${mobileCheck?'api':'web'}.whatsapp.com/send?phone=5561981087386&text=${msg}`);
+        window.open(`https://api.whatsapp.com/send?phone=5561981087386&text=${msg}`);
     }
 
-    const mobileCheck = ()=> {
-        let check = false;
-        (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
-        return check;
-    };
     const addPet = () => {
         setPets([...pets, new Date().getTime()]);
     }
@@ -345,12 +340,21 @@ export default () => {
 
                 <button type="submit">
                     <span>
-                        Enviar solicitação <FaPaperPlane />
+                        Enviar solicitação e concordar com o contrato <FaPaperPlane />
                     </span>
                     <span className="currency">
                         {valorTotal}
                     </span>
                 </button>
+                <p>
+                Ao solicitar a reserva você estará de acordo com os
+                <u><a href="https://hotels.wixapps.net/index.html?cacheKiller=1594056360090&commonConfig=%7B%22brand%22%3A%22wix%22%7D&compId=TPASection_k9ybd1w1&currency=BRL&deviceType=desktop&height=689&instance=e5WVCNh1iH6IOahFww_m0QJ_KSiY7jAcW9C_9ShbBXU.eyJpbnN0YW5jZUlkIjoiNmQ4ZWM1ZWUtNDM5Zi00ZDg3LWIyMDYtNzhlY2M2YjVjNjNjIiwiYXBwRGVmSWQiOiIxMzVhYWQ4Ni05MTI1LTYwNzQtNzM0Ni0yOWRjNmEzYzliY2YiLCJtZXRhU2l0ZUlkIjoiYjc3NTBhY2EtMTQ2NC00MjQwLWEyY2YtMzNlYmU3YzZmYzkwIiwic2lnbkRhdGUiOiIyMDIwLTA3LTA2VDIwOjU1OjUyLjcxMloiLCJ1aWQiOiI1YjM3ZTI2YS1jNjc4LTRhMTYtOGJjYS1hNzA2NDcwNmRkNGYiLCJwZXJtaXNzaW9ucyI6Ik9XTkVSIiwidmVuZG9yUHJvZHVjdElkIjoiaG90ZWxzIiwiZGVtb01vZGUiOmZhbHNlLCJiaVRva2VuIjoiZGFmYmNmMjQtNTdmYi0wZmM3LTEwYzktNGIwNzIxNzMzYWFjIiwic2l0ZU93bmVySWQiOiI1YjM3ZTI2YS1jNjc4LTRhMTYtOGJjYS1hNzA2NDcwNmRkNGYiLCJzaXRlTWVtYmVySWQiOiI1YjM3ZTI2YS1jNjc4LTRhMTYtOGJjYS1hNzA2NDcwNmRkNGYiLCJleHBpcmF0aW9uRGF0ZSI6IjIwMjAtMDctMDdUMDA6NTU6NTIuNzEyWiIsImxvZ2luQWNjb3VudElkIjoiNWIzN2UyNmEtYzY3OC00YTE2LThiY2EtYTcwNjQ3MDZkZDRmIn0&locale=pt&pageId=bmoxw&section-url=https%3A%2F%2Fhotels.wixapps.net%2Findex.html&siteRevision=298&target=_self&tz=America%2FSao_Paulo&viewMode=preview&viewerCompId=TPASection_k9ybd1w1&width=980&userLanguage=pt#/terms/78ada2c5-ab18-4bb0-ab29-20b27378b128"
+                rel="noopener noreferrer"
+                target="_blank"
+                style={{color:'#000000'}}
+                >Dados do contrato</a></u>
+                </p>
+                
             </form>
         </>
     )
